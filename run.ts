5420c1e4e456e856282c74141f8222bb0484972c
Oscar Otero <oom@oscarotero.com>
2025-06-19 12:57:26 +0200

    changes

--- run.ts
+++ run.ts
@@ -1,10 +1,12 @@
-const decoder = new TextDecoder();
 const gitUrl = Deno.env.get("GIT_URL");
 const gitUsername = Deno.env.get("GIT_USERNAME");
 const gitPassword = Deno.env.get("GIT_PASSWORD");
+const gitEmail = Deno.env.get("GIT_EMAIL");
 
-if (!gitUrl || !gitUsername || !gitPassword) {
-  console.error("GIT_URL, GIT_USERNAME, and GIT_PASSWORD must be set.");
+if (!gitUrl || !gitUsername || !gitPassword || !gitEmail) {
+  console.error(
+    "GIT_URL, GIT_USERNAME, GIT_PASSWORD, and GIT_EMAIL must be set.",
+  );
   Deno.exit(1);
 }
 
@@ -20,27 +22,20 @@ for (const entry of entries) {
   Deno.removeSync(entry.name, { recursive: true });
 }
 
-const result = await runCommand("git", [
-  "clone",
-  "--depth=1",
-  gitRepo.toString(),
-  ".",
-]);
+// Clone the repository
+await run("git", "clone", "--depth=1", gitRepo.toString(), ".");
 
-console.log(result);
+// Configure Git user
+await run("git", "config", "user.name", gitUsername);
+await run("git", "config", "user.email", gitEmail);
+await run("git", "config", "pull.rebase", "false");
 
-async function runCommand(cmd: string, args: string[] = []) {
+async function run(cmd: string, ...args: string[]) {
   const command = new Deno.Command(cmd, {
     args,
-    stdout: "piped",
-    stderr: "piped",
+    stdout: "inherit",
+    stderr: "inherit",
   });
 
-  const { stdout, stderr } = await command.output();
-
-  if (stderr) {
-    console.error(decoder.decode(stderr));
-  }
-
-  return decoder.decode(stdout);
+  await command.output();
 }
